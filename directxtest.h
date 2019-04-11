f93bb6a8c00d7dc17a3ba9af59ffb1c86ffdf650
Chuck Walbourn <walbourn@users.noreply.github.com>
2019-04-10 22:52:06 -0700

    Added VS 2019 Desktop projects (#3)

--- directxtest.h
+++ directxtest.h
@@ -4,6 +4,24 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 //-------------------------------------------------------------------------------------
 
+#pragma once
+
+#pragma warning(push)
+#pragma warning(disable : 4005)
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#define NODRAWTEXT
+#define NOGDI
+#define NOMCX
+#define NOSERVICE
+#define NOHELP
+#pragma warning(pop)
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#include <stdio.h>
+
 #include <windows.h>
 #include <stdio.h>
 
