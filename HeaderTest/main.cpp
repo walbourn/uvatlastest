faccc24e4f3da2d73c72e86e6b62c1cac6581abe
Chuck Walbourn <chuckw@microsoft.com>
2020-06-03 14:19:10 -0700

    Added HeaderTest

---
+++ HeaderTest/main.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// This test ensures that all public headers fully include all their dependancies, as well compile cleanly at maximum warning level
+
+#include "UVAtlas.h"
+
+int main()
+{
+    return 0;
+}
