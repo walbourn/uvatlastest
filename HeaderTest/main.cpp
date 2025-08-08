e9d1393d798cce6f2592e4a08ff9e341f6cedee3
Chuck Walbourn <chuckw@microsoft.com>
2025-08-07 18:23:14 -0700

    CMake updates

--- HeaderTest/main.cpp
+++ HeaderTest/main.cpp
@@ -4,13 +4,14 @@
 // This test ensures that all public headers fully include all their dependancies, as well compile cleanly at maximum warning level
 
 #ifdef _MSC_VER
-#pragma warning(disable : 4619 4616 4061 4365 4668 4710 4820 5031 5032 5039)
+#pragma warning(disable : 4619 4616 4061 4365 4668 4710 4820 4865 5031 5032 5039)
 // C4619/4616 #pragma warning warnings
 // C4061 enumerator 'X' in switch of enum 'X' is not explicitly handled by a case label
 // C4365 signed/unsigned mismatch
 // C4668 not defined as a preprocessor macro
 // C4710 function not inlined
 // C4820 padding added after data member
+// C4865 the underlying type will change from 'int' to 'unsigned int' when '/Zc:enumTypes' is specified on the command line
 // C5031/5032 push/pop mismatches in windows headers
 // C5039 pointer or reference to potentially throwing function passed to extern C function under - EHc
 #endif
